6a2802b086fb8f7bc587cb4e67e8a54ca5f21ac7
unknown <williamsartijose@hotmail.com>
2023-01-14 16:42:42 +0000

    Iniciando o Projeto

---
+++ src/main/java/com/wsjsistema/wsjdscommerce/WsjdscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.wsjsistema.wsjdscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsjdscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsjdscommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsjsistema/wsjdscommerce/WsjdscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsjsistema.wsjdscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsjdscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
