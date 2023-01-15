2097b49f623b16644e556c6fd2cb5475d87038fb
unknown <williamsartijose@hotmail.com>
2023-01-15 18:44:23 +0000

    Criando a Classe Order e Enum, Faznedo o relacionamento muitos-para-um @ManyToOne

---
+++ src/main/java/com/wsjsistema/wsjdscommerce/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.wsjsistema.wsjdscommerce.entities;
+
+public enum OrderStatus {
+	
+	WATTING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+
+}
--- src/main/java/com/wsjsistema/wsjdscommerce/entities/User.java
+++ src/main/java/com/wsjsistema/wsjdscommerce/entities/User.java
@@ -1,11 +1,14 @@
 package com.wsjsistema.wsjdscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User {
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
+	
+	@OneToMany(mappedBy = "client") // 1(um) para muitos
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -94,6 +100,11 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public User(List<Order> orders) {
+		super();
+		this.orders = orders;
 	}
 	
 	
