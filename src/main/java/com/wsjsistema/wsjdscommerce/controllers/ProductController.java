c110a5e6ee68ad4b76e4e15b66a2aeaae9cafe14
unknown <williamsartijose@hotmail.com>
2023-04-11 21:26:37 +0100

    Metodo verbo Get exemplo

---
+++ src/main/java/com/wsjsistema/wsjdscommerce/controllers/ProductController.java
@@ -0,0 +1,17 @@
+package com.wsjsistema.wsjdscommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value = "/products")
+public class ProductController {
+
+    @GetMapping
+    public String teste(){
+        return "Ola Mundo!";
+    }
+
+
+}
