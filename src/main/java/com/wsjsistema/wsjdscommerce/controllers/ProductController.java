806752c42a2ff387b4f6dd52c7cec8862b3ffd03
unknown <williamsartijose@hotmail.com>
2023-04-14 22:41:38 +0100

    GET Busca paginada de produtos

--- src/main/java/com/wsjsistema/wsjdscommerce/controllers/ProductController.java
+++ src/main/java/com/wsjsistema/wsjdscommerce/controllers/ProductController.java
@@ -5,11 +5,14 @@ import com.wsjsistema.wsjdscommerce.entities.Product;
 import com.wsjsistema.wsjdscommerce.repositories.ProductRepository;
 import com.wsjsistema.wsjdscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -22,6 +25,13 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id){
         ProductDTO dto = service.findByIDto(id);
         return dto;
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
+
+
     }
 
 
--- src/main/java/com/wsjsistema/wsjdscommerce/services/ProductService.java
+++ src/main/java/com/wsjsistema/wsjdscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.wsjsistema.wsjdscommerce.dto.ProductDTO;
 import com.wsjsistema.wsjdscommerce.entities.Product;
 import com.wsjsistema.wsjdscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,4 +27,14 @@ public class ProductService {
         return  dto;
 
     }
+//Buscando todos sem ser paginados
+//    @Transactional(readOnly = true)
+//    public List<ProductDTO>  findAll() {
+//        List<Product> result = repository.findAll();
+//        return  result.stream().map(x -> new ProductDTO(x)).toList();
+@Transactional(readOnly = true)
+public Page<ProductDTO> findAll(Pageable pageable) {
+    Page<Product> result = repository.findAll(pageable);
+    return result.map((x -> new ProductDTO(x)));
+}
 }
