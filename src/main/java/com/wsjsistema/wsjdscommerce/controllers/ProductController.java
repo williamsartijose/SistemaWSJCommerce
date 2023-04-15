03d9ae9a14a5d2d633687a82ecfcd9ff18c316dd
unknown <williamsartijose@hotmail.com>
2023-04-15 22:45:10 +0100

    Validação com Bean Validation

--- src/main/java/com/wsjsistema/wsjdscommerce/controllers/ProductController.java
+++ src/main/java/com/wsjsistema/wsjdscommerce/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.wsjsistema.wsjdscommerce.dto.ProductDTO;
 import com.wsjsistema.wsjdscommerce.entities.Product;
 import com.wsjsistema.wsjdscommerce.repositories.ProductRepository;
 import com.wsjsistema.wsjdscommerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -34,7 +35,7 @@ public class ProductController {
     }
 
     @PostMapping
-        public ResponseEntity <ProductDTO> insert(@RequestBody ProductDTO dto){
+        public ResponseEntity <ProductDTO> insert(@Valid  @RequestBody ProductDTO dto){
             dto = service.insert(dto);
             URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                     .buildAndExpand(dto.getId()).toUri();
@@ -42,7 +43,7 @@ public class ProductController {
         }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto ){
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto ){
          dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
