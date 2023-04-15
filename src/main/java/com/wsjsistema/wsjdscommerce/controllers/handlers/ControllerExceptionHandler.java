5bb012f1954d510a0fb9a9166d68dc75cf757306
unknown <williamsartijose@hotmail.com>
2023-04-15 23:09:49 +0100

    Customizando a resposta da validação

--- src/main/java/com/wsjsistema/wsjdscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/wsjsistema/wsjdscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.wsjsistema.wsjdscommerce.controllers.handlers;
 
 import com.wsjsistema.wsjdscommerce.dto.CustomError;
+import com.wsjsistema.wsjdscommerce.dto.ValidationError;
 import com.wsjsistema.wsjdscommerce.services.execptions.DatabaseException;
 import com.wsjsistema.wsjdscommerce.services.execptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,6 +27,17 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> customName(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(),e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados Inválidos", request.getRequestURI());
+       for (FieldError f : e.getBindingResult().getFieldErrors()) {
+           err.addError(f.getField(), f.getDefaultMessage());
+       }
         return ResponseEntity.status(status).body(err);
     }
 }
