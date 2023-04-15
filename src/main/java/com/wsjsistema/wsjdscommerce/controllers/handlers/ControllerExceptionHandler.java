bd1daafb436f1ac83d7fd7e3c229488891ed7ff3
unknown <williamsartijose@hotmail.com>
2023-04-15 15:25:16 +0100

    Tratando exceção com resposta customizada

---
+++ src/main/java/com/wsjsistema/wsjdscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.wsjsistema.wsjdscommerce.controllers.handlers;
+
+import com.wsjsistema.wsjdscommerce.dto.CustomError;
+import com.wsjsistema.wsjdscommerce.services.execptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> customName(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(),e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
